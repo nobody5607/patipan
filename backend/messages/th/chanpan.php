75da5dbb120fcf64e4807805d6b33b535a461542
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2019-10-07 01:08:05 +0700

    fix

---
+++ backend/messages/th/chanpan.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Are you sure you want to delete this item?' => 'คุณแน่ใจหรือว่าต้องการลบรายการนี้หรือไม่?',
+    'Are you sure you want to delete these items?' => 'คุณแน่ใจหรือว่าต้องการลบรายการเหล่านี้?'
+];
